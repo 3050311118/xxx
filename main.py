2d49fc3fcf915b85d5d5d73f02c1f9606bb3ae61
3050311118 <844725532@qq.com>
2021-11-18 08:54:51 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+def on_forever():
+    print("afwafawfwaef")
+forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+forever(function () {
+    console.log("afwafawfwaef\\n")
+    pause(1000)
+    pins.P16.digitalWrite(false)
+})
